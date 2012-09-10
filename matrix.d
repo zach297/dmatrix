c17bc6f48c6f3e1b68e92eb1411d36121ef2e993
zach297 <zach297@gmail.com>
2012-09-10 02:00:20 -0500

    add convenience functions

--- matrix.d
+++ matrix.d
@@ -1,26 +1,50 @@
 /*
 Copyright (C) 2012 Zach Reizner (zach297@gmail.com)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 import std.algorithm;
-import std.math; 
+import std.math;
 import std.random;
 import std.stdio;
 import vector;
 
-struct Matrix(T, size_t S=4)
+struct Matrix(T, size_t S=4) if (S>=1)
 {
     private alias Matrix!(T, S) mat;
     private alias Vector!(T, S) vec;
 
     private T[S][S] A;
-    
+
+    this(T[S*S] V ...)
+    {
+        foreach(x; 0..S)
+            foreach(y; 0..S)
+                A[x][y] = V[x*S+y];
+    }
+    this(T[S*S] V)
+    {
+        foreach(x; 0..S)
+            foreach(y; 0..S)
+                A[x][y] = V[x*S+y];
+    }
+
     static mat opCall(T[S*S] V)
     {
         mat A;
@@ -29,7 +53,7 @@ struct Matrix(T, size_t S=4)
                 A[x,y] = V[x*S+y];
         return A;
     }
-    
+
     @property T[] array()
     {
         T[] V = new T[S*S];
@@ -39,13 +63,13 @@ struct Matrix(T, size_t S=4)
         return V;
     }
     @property T* ptr() { return A[0].ptr; }
-    
+
     T opIndex(size_t x, size_t y) { return A[x][y]; }
     T opIndexAssign(T v, size_t x, size_t y)
     {
         return A[x][y] = v;
     }
-    
+
     ref mat opOpAssign(string op)(T B)
     {
         foreach(x; 0..S)
@@ -53,7 +77,7 @@ struct Matrix(T, size_t S=4)
                 mixin("A[x][y] " ~ op ~ "= B;");
         return this;
     }
-    
+
     ref mat opOpAssign(string op)(mat B)
     {
         static if (op == "*")
@@ -61,9 +85,9 @@ struct Matrix(T, size_t S=4)
             A = (this * B).A;
             return this;
         }
-        
+
     }
-    
+
     mat opBinary(string op)(T B)
     {
         mat R;
@@ -72,7 +96,7 @@ struct Matrix(T, size_t S=4)
                 mixin("R.A[x][y] = A[x][y] " ~ op ~ " B;");
         return R;
     }
-    
+
     vec opBinary(string op)(vec B)
     {
         static if (op == "*")
@@ -80,7 +104,7 @@ struct Matrix(T, size_t S=4)
             return multiply(B);
         }
     }
-    
+
     mat opBinary(string op)(mat B)
     {
         static if (op == "*")
@@ -88,20 +112,20 @@ struct Matrix(T, size_t S=4)
             return multiply(B);
         }
     }
-    
+
     void zero()
     {
         foreach(i; 0..S)
             A[i][] = 0;
     }
-    
+
     void identity()
     {
         foreach(x; 0..S)
             foreach(y; 0..S)
                 A[x][y] = (x==y) ? 1 : 0;
     }
-    
+
 
     vec multiply(vec B)
     {
@@ -113,7 +137,7 @@ struct Matrix(T, size_t S=4)
         }
         return R;
     }
-    
+
     mat multiply(mat B)
     {
         mat R;
@@ -129,7 +153,7 @@ struct Matrix(T, size_t S=4)
         }
         return R;
     }
-    
+
     void print()
     {
         foreach(y; 0..S)
@@ -141,10 +165,10 @@ struct Matrix(T, size_t S=4)
             writeln();
         }
     }
-    
-    static mat createPerspective(real aspect, real fov, real near, real far)
+
+    static Matrix!(T,4) createPerspective(real aspect, real fov, real near, real far)
     {
-        mat A;
+        Matrix!(T,4) A;
         A.zero();
         real f = 1/tan(fov / 2);
         real diff = near - far;
@@ -155,28 +179,46 @@ struct Matrix(T, size_t S=4)
         A[2,3] = -1;
         return A;
     }
-    
-    static Matrix!(T,3) createRotation()
+
+    static Matrix!(T,4) createViewing(vec3 position, vec3 forward, vec3 up)
     {
-        Matrix!(T,3) rot;
-        
-        return rot;
+        alias forward f;
+        alias position p;
+        forward.normalize();
+        up.normalize();
+        vec3 s = vec3.cross(f, up);
+        vec3 u = vec3.cross(s, f);
+        Matrix!(T,4) A;
+        A.identity();
+        A[0,0] = s[0];
+        A[1,0] = s[1];
+        A[2,0] = s[2];
+        A[0,1] = u[0];
+        A[1,1] = u[1];
+        A[2,1] = u[2];
+        A[0,2] = -f[0];
+        A[1,2] = -f[1];
+        A[2,2] = -f[2];
+        A[3,0] = p[0];
+        A[3,1] = p[1];
+        A[3,2] = p[2];
+        return A;
     }
-    
+
     static mat createZero()
     {
         mat A;
         A.zero();
         return A;
     }
-    
+
     static mat createIdentity()
     {
         mat A;
         A.identity();
         return A;
     }
-    
+
     static mat createRandom()
     {
         mat A;
@@ -184,13 +226,51 @@ struct Matrix(T, size_t S=4)
         {
             foreach(y; 0..S)
             {
-                A[x,y] = uniform(0.0, 1.0);
+                A[x,y] = uniform(-1.0, 1.0);
             }
         }
         return A;
     }
-    
-    
+
+    static mat createTranslation(size_t Q)(Vector!(T,Q) translation) if (Q==S-1)
+    {
+        mat ret = mat.createIdentity();
+
+        foreach(y; 0..Q)
+            ret.A[$-1][y] = translation[y];
+
+        return ret;
+    }
+
+    static mat createUniformScale(T scale)
+    {
+        mat ret = mat.createIdentity();
+
+        foreach(y; 0..S)
+            ret.A[y][y] = scale;
+
+        return ret;
+    }
+
+    static mat createScale(size_t Q)(Vector!(T,Q) scale) if (Q==S-1)
+    {
+        mat ret = mat.createIdentity();
+
+        foreach(y; 0..Q)
+            ret.A[y][y] = scale[y];
+
+        return ret;
+    }
+
+    static Matrix!(T,4) createRotation()
+    {
+        Matrix!(T,4) rot = Matrix!(T,4).createIdentity();
+
+
+
+        return rot;
+    }
+
 }
 
 alias Matrix!(float,2) mat2;
@@ -203,34 +283,34 @@ unittest
     mat4 zeroMatrix;
     zeroMatrix.zero();
     assert(!canFind!"a != b"(zeroMatrix.array, 0));
-    
+
     // Test the identity matrix.
     mat4 A = mat4.createIdentity();
     mat4 B = mat4.createRandom();
     assert(B == A*B);
-    
+
     // Test that the matrix is arranged in column major order.
     assert(B[0, 1] == B.ptr[1]);
-    
+
     // Test the perspective matrix.
     mat4 P = mat4.createPerspective(4.0/3.0, PI_4, 0.1, 100);
     assert(equal!"abs(a-b) < float.epsilon"(P.array, [1.8106601238250732, 0, 0, 0, 0, 2.4142136573791504, 0, 0, 0, 0, -1.0020020008087158, -1, 0, 0, -0.20020020008087158, 0]));
-    
+
     // Test a basic matrix multiplication.
     mat4 D = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     mat4 E = [100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85];
     assert((D*E).array == [2344, 2738, 3132, 3526, 2248, 2626, 3004, 3382, 2152, 2514, 2876, 3238, 2056, 2402, 2748, 3094]);
     D *= A;
     assert(D.array == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
-    
+
     // Test multiply by a scalar.
     assert((D*2).array == [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]);
-    
+
     // Test add and assign.
     mat2 F = [-1, 1, -2, 2];
     F += 2;
     assert(F.array == [1, 3, 0, 4]);
-    
+
     // Test a matrix multiply with a vector.
     vec4 G = [-4, 4, 4, 4];
     assert((D*G).array == [96,104,112,120]);
--- vector.d
+++ vector.d
@@ -1,38 +1,52 @@
 /*
 Copyright (C) 2012 Zach Reizner (zach297@gmail.com)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 import std.algorithm;
+import std.math;
+import std.random;
 import std.stdio;
 import matrix;
 
-struct Vector(T, size_t S=4)
+struct Vector(T, size_t S=4) if (S>=1)
 {
     private alias Vector!(T,S) vec;
     private alias Matrix!(T,S) mat;
-    
+
     private T[S] V = 0;
-    
+
+    this(T[S] B ...) { V = B; }
     this(T[S] B) { V = B; }
-    
+
     @property T[] array() { return V; }
-    
+
     static vec opCall(T[S] V)
     {
         vec U;
         U.V = V;
         return U;
     }
-    
+
     ref T opIndex(size_t i) { return V[i]; }
     T opIndexAssign(T v, size_t i) { return V[i] = v; }
- 
+
     ref vec opOpAssign(string op)(T B)
     {
         foreach (i; 0..S)
@@ -41,7 +55,7 @@ struct Vector(T, size_t S=4)
         }
         return this;
     }
- 
+
     ref vec opOpAssign(string op)(vec B)
     {
         foreach (i; 0..S)
@@ -50,7 +64,18 @@ struct Vector(T, size_t S=4)
         }
         return this;
     }
- 
+
+    vec opUnary(string op)()
+    {
+        static if (op == "-")
+        {
+            vec R = this;
+            foreach (i; 0..S)
+                R.V[i] = -R.V[i];
+            return R;
+        }
+    }
+
     vec opBinary(string op)(T B)
     {
         vec R;
@@ -60,7 +85,7 @@ struct Vector(T, size_t S=4)
         }
         return R;
     }
-    
+
     vec opBinary(string op)(vec B)
     {
         vec R;
@@ -70,12 +95,25 @@ struct Vector(T, size_t S=4)
         }
         return R;
     }
-    
+
+    @property T lengthSquared() { return reduce!"a+(b*b)"(0.0, V); }
+    @property T length() { return sqrt(reduce!"a+(b*b)"(0.0, V)); }
+
+    @property vec normal()
+    {
+        return this / this.length;
+    }
+
+    void normalize()
+    {
+        this /= this.length;
+    }
+
     T dot(vec B)
     {
         return reduce!"a+b"((this * B).V);
     }
-    
+
     static Vector!(T,3) cross(Vector!(T,3) A, Vector!(T,3) B)
     {
         return Vector!(T,3)(
@@ -83,7 +121,7 @@ struct Vector(T, size_t S=4)
              A.V[2]*B.V[0] - A.V[0]*B.V[2],
              A.V[0]*B.V[1] - A.V[1]*B.V[0]]);
     }
-    
+
     void print()
     {
         write("vec", S, "(");
@@ -95,8 +133,16 @@ struct Vector(T, size_t S=4)
         }
         writeln(")");
     }
-    
 
+    static vec createRandom()
+    {
+        vec A;
+        foreach(x; 0..S)
+        {
+            A[x] = uniform(-1.0, 1.0);
+        }
+        return A;
+    }
 }
 
 alias Vector!(float,2) vec2;
@@ -107,38 +153,49 @@ unittest
 {
     // Test the static init.
     vec4 zeroes;
-    assert(zeroes[0] == 0);
-    
+    assert(zeroes.V == [0,0,0,0]);
+
+    // Test init from params;
+    vec4 sequence = vec4(1,2,3,4);
+    assert(sequence.V == [1,2,3,4]);
+
     // Test init from an array.
     vec4 A = [5, 1, 2, 3];
     assert(A.V == [5,1,2,3]);
-    
+
     // Test multiply by a scalar .
     assert((A * 2).V == [10,2,4,6]);
-    
+
     // Test multiply by a vector.
     vec4 B = [10, 8, 6, 4];
     assert((A*B).V == [50, 8, 12, 12]);
     assert((A*B) == (B*A));
-    
+
     // Test subtract and assign with a scalar
     vec4 S = [0, -3, -7, -5];
     S -= 0.5;
     assert(S.V == [-0.5, -3.5, -7.5, -5.5]);
-    
+
     // Test add and assign with vector.
     vec4 C = [10, 10, 10, 10];
     (C += B)[0] = -1; // Also test that it returns a ref.
     assert(C.V == [-1, 18, 16, 14]);
-    
+
     // Test dot product of perpendicular vectors.
     vec3 D = [1, 0, 0];
     vec3 E = [0, 1, 0];
     assert(D.dot(E) == 0);
-    
+
     // Test cross product of perpendicular vectors.
     assert(vec3.cross(D, E).V == [0, 0, 1]);
-    
 
-    
+    // Test a negation.
+    assert((-C).V == [1, -18, -16, -14]);
+
+    // Test the lengthSquared and length.
+    assert(abs(A.lengthSquared - 39) < float.epsilon);
+    assert(abs(A.length - 6.2449979983983982058468931209398) < float.epsilon);
+
+    // Test getting the normal.
+    assert(abs(A.normal.length - 1) < float.epsilon);
 }
